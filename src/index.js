b10febeec1421c10396ad7a3b7f6769643c72c1c
Mad Pillow <dmtr.schv@gmail.com>
2021-02-07 11:07:53 +0200

    init: load first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        array.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (str.includes(array[i])) {
+            str = str.replace(array[i], "");
+            i = -1;
+        }
+    }
+    if (str.length != 0) {
+        return false;
+    } else {
+        return true;
+    }
+};
